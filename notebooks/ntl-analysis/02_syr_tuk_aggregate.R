be5d93d68d5bb3317ad0553c81753d3b8b553ab8
Rob Marty <ramarty@email.wm.edu>
2023-05-27 17:14:36 -0400

    add border

---
+++ notebooks/ntl-analysis/02_syr_tuk_aggregate.R
@@ -0,0 +1,146 @@
+# Trends in Governorate NTL
+
+# Files for ADM 3 and 4, and for the two types of daily data (1 = less processed;
+# 2 = more processed)
+
+DELETE_FILES <- F
+
+if(DELETE_FILES){
+  f_to_rm <- file.path(ntl_bm_dir, "FinalData", "aggregated", "syr_temp", "daily_files") %>%
+    list.files(full.names = T,
+               pattern = "*.Rds")
+  
+  for(f_i in f_to_rm){
+    file.remove(f_i)
+  }
+}
+
+for(adm_i in c(2, 3, 4)){
+  for(product_id in c("VNP46A3", "VNP46A2")){
+    
+    # Load/prep gas flaring data ---------------------------------------------------
+    #### Gas Flaring
+    gf_df <- readRDS(file.path(gas_flare_dir, "FinalData", "gas_flare_locations.Rds"))
+    
+    coordinates(gf_df) <- ~longitude+latitude
+    crs(gf_df) <- CRS("+init=epsg:4326")
+    
+    gf_sf <- gf_df %>% st_as_sf() %>% st_buffer(dist = 5000)
+    gf_sp <- gf_sf %>% as("Spatial")
+    
+    ## Non GS Locations
+    gadm_0_sp <- read_sf(file.path(unocha_dir, "RawData", "syr_admbnda_adm0_uncs_unocha_20201217.json")) %>%
+      as("Spatial")
+    gadm_0_sp$date <- NULL
+    
+    gadm_no_gf_sp <- gDifference(gadm_0_sp, gf_sp, byid=F)
+    gadm_no_gf_sp$id <- 1
+    
+    # Load data --------------------------------------------------------------------
+    if(adm_i == 2){
+      gadm_sp <- read_sf(file.path(unocha_dir, "RawData", "syr_admbnda_adm2_uncs_unocha_20201217.json")) %>%
+        as("Spatial")
+      gadm_sp$date <- NULL
+    }
+    
+    if(adm_i == 3){
+      gadm_sp <- read_sf(file.path(unocha_dir, "RawData", "syr_admbnda_adm3_uncs_unocha_20201217.json")) %>%
+        as("Spatial")
+      gadm_sp$date <- NULL
+    }
+    
+    # Point file, so create circles with 0.5km radius
+    if(adm_i == 4){
+      gadm_sp <- read_sf(file.path(unocha_dir, "RawData", "syr_pplp_adm4_unocha_20210113.json")) %>%
+        as("Spatial")
+      gadm_sp$date <- NULL
+      
+      gadm_sp <- geo.buffer(gadm_sp, r = 0.5*1000)
+      
+      gadm_sp <- raster::aggregate(gadm_sp, by = "ADM4_PCODE")
+    }
+    
+    gadm_df <- gadm_sp@data %>%
+      dplyr::mutate(uid = 1:n())
+    
+    gadm_id_df <- gadm_df %>%
+      dplyr::select(uid)
+    
+    # Extract VIIRS - Black Marble -------------------------------------------------
+    daily_files <- list.files(file.path(ntl_bm_dir, "FinalData", paste0(product_id, "_rasters")))
+    
+    for(file_i in rev(daily_files)){
+      
+      OUT_FILE <- file.path(ntl_bm_dir, "FinalData", "aggregated", 
+                            "syr_temp", product_id,
+                            paste0("syr_admin", 
+                                   adm_i, "_", 
+                                   file_i %>% str_replace_all(".tif", ".Rds")))
+      
+      if(!file.exists(OUT_FILE)){
+        
+        print(file_i)
+        
+        r <- raster(file.path(ntl_bm_dir, "FinalData", paste0(product_id, "_rasters"), file_i))
+        
+        r_gf   <- r %>% crop(gf_sp) %>% mask(gf_sp)
+        r_nogf <- r %>% crop(gadm_no_gf_sp) %>% mask(gadm_no_gf_sp)
+        
+        gadm_sf <- gadm_sp %>% st_as_sf()
+        
+        gadm_id_df$viirs_bm_mean <- exact_extract(r, gadm_sf, 'mean')
+        gadm_id_df$viirs_bm_sum <- exact_extract(r, gadm_sf, 'sum')
+        gadm_id_df$viirs_bm_median <- exact_extract(r, gadm_sf, 'median')
+        
+        gadm_id_df$viirs_bm_gf_mean <- exact_extract(r_gf, gadm_sf, 'mean')
+        gadm_id_df$viirs_bm_gf_sum  <- exact_extract(r_gf, gadm_sf, 'sum')
+        
+        gadm_id_df$viirs_bm_nogf_mean <- exact_extract(r_nogf, gadm_sf, 'mean')
+        gadm_id_df$viirs_bm_nogf_sum  <- exact_extract(r_nogf, gadm_sf, 'sum')
+        
+        # Add date info
+        if(product_id == "VNP46A2"){
+          year_i <- file_i %>% substring(12,15) %>% as.numeric()
+          day_i  <- file_i %>% substring(17,19) %>% as.numeric()
+          gadm_id_df$year <- year_i
+          gadm_id_df$day  <- day_i
+          gadm_id_df$date <- as.Date(day_i, origin = paste0(year_i-1, "-12-31")) # 1 = Jan 1 of next year
+        }
+        
+        if(product_id == "VNP46A3"){
+          year_i <- file_i %>% substring(12,15) %>% as.numeric()
+          month_i  <- file_i %>% substring(17,19) %>% as.numeric()
+          gadm_id_df$year <- year_i
+          gadm_id_df$month  <- month_i
+          gadm_id_df$date <- ymd(paste0(year_i, "-", month_i, "-01")) # 1 = Jan 1 of next year
+        }
+        
+        #### Merge NTL with GADM data 
+        data_df <- gadm_id_df %>%
+          left_join(gadm_df, by = c("uid"))
+        
+        data_df$viirs_bm_mean[data_df$viirs_bm_sum == 0]           <- 0
+        data_df$viirs_bm_gf_mean[data_df$viirs_bm_gf_sum == 0]     <- 0
+        data_df$viirs_bm_nogf_mean[data_df$viirs_bm_nogf_sum == 0] <- 0
+        
+        # Export -----------------------------------------------------------------------
+        saveRDS(data_df, OUT_FILE)
+      }
+      
+      # viirs_bm_df <- map_df(daily_files, function(file_i){ # nrow(ym_df)
+      # 
+      #   
+      #   return(gadm_id_df)
+      # })
+      
+      
+      
+      # # Export -----------------------------------------------------------------------
+      # write_csv(data_df, file.path(ntl_bm_dir, "FinalData", "aggregated", 
+      #                              paste0("syr_admin_daily",
+      #                                     adm_i, "_", product_id, ".csv")))
+      
+      
+    }
+  }
+}
--- notebooks/ntl-analysis/04_maps_ntl_by_eq_intensity.R
+++ notebooks/ntl-analysis/04_maps_ntl_by_eq_intensity.R
@@ -1,19 +1,69 @@
 # Maps
 
-# Load / clean NTL data --------------------------------------------------------
-ntl_df <- read_csv(file.path(ntl_bm_dir, "FinalData", "aggregated", "syr_temp", 
-                             "syr_admin_adm3_daily.csv"))
+# Load / clean NTL data ========================================================
 
-eq3_df <- read_xlsx(file.path(eq_intensity_dir, "RawData", "Intensity_ADM3.xlsx"))
-eq3_df <- eq3_df %>%
-  dplyr::select(ADM3_CODE, POPULATION, intensity) %>%
-  dplyr::rename(ADM3_PCODE = ADM3_CODE)
+# Lights -----------------------------------------------------------------------
+ntl_syr_df <- read_csv(file.path(ntl_bm_dir, "FinalData", "aggregated", "syr_temp", 
+                                 "syr_admin_adm3_daily.csv"))
+
+ntl_tur_df <- readRDS(file.path(data_tur_dir, "NTL BlackMarble", "FinalData", "aggregated",
+                                paste0("adm2", "_", "VNP46A2", ".Rds")))
+
+ntl_syr_df <- ntl_syr_df %>%
+  mutate(adm = ADM3_PCODE) %>%
+  dplyr::select(viirs_bm_mean, viirs_bm_gf_mean, adm, date)
+
+ntl_tur_df <- ntl_tur_df %>%
+  dplyr::select(ntl_bm_mean, ntl_bm_gf_mean, pcode, date) %>%
+  dplyr::rename(viirs_bm_mean = ntl_bm_mean,
+                viirs_bm_gf_mean = ntl_bm_gf_mean,
+                adm = pcode)
+
+ntl_df <- bind_rows(ntl_syr_df,
+                    ntl_tur_df)
+
+# Earthquake -------------------------------------------------------------------
+eq_syr_df <- read_csv(file.path(data_dir, "Earthquake Intensity by admin regions", "syria_adm3_earthquake_intensity.csv"))
+eq_tur_df <- read_csv(file.path(data_dir, "Earthquake Intensity by admin regions", "turkiye_admin2_earthquake_intensity_max.csv"))
+
+syr_pcode <- read_csv(file.path(ntl_bm_dir, "FinalData", "aggregated", "syr_temp", 
+                                "syr_admin_adm3_daily.csv")) %>%
+  distinct(ADM2_EN, ADM3_EN, ADM3_PCODE) %>%
+  dplyr::mutate(adm23 = paste(ADM2_EN, ADM3_EN),
+                adm = ADM3_PCODE) %>%
+  dplyr::select(adm23, adm)
+
+eq_syr_df <- eq_syr_df %>%
+  dplyr::mutate(adm23 = paste(ADM2_EN, ADM3_EN)) %>%
+  left_join(syr_pcode, by = "adm23") %>%
+  dplyr::select(max_intensity_feb6, adm) %>%
+  dplyr::rename(mi = max_intensity_feb6)
+
+eq_tur_df <- eq_tur_df %>%
+  dplyr::rename(adm = pcode,
+                mi = mmi_feb06_7p8) %>%
+  dplyr::select(adm, mi)
+
+eq_df <- bind_rows(eq_syr_df,
+                   eq_tur_df)
+
+eq_df <- eq_df %>%
+  mutate(intensity = case_when(
+    mi >= 7 ~ "Very Strong",
+    mi >= 6 ~ "Strong",
+    mi >= 5 ~ "Moderate",
+    mi >= 4 ~ "Light",
+    mi >= 3 ~ "Weak",
+    mi >= 2 ~ "Weak",
+    mi < 2 ~ "Not felt"
+  ))
+
+# Append NTL / MI --------------------------------------------------------------
+ntl_df <- ntl_df %>%
+  left_join(eq_df, by = "adm")
 
 ntl_df <- ntl_df %>%
-  left_join(eq3_df, by = "ADM3_PCODE")
-
-ntl_df <- ntl_df %>%
-  group_by(ADM3_PCODE, intensity) %>%
+  group_by(adm, intensity) %>%
   summarise(viirs_bm_mean_jan = mean(viirs_bm_mean[date %in% ymd("2023-01-01"):ymd("2023-01-31")]),
             viirs_bm_mean_3d = mean(viirs_bm_mean[date %in% ymd("2023-02-07"):ymd("2023-02-09")]),
             viirs_bm_mean_14d = mean(viirs_bm_mean[date %in% ymd("2023-02-07"):ymd("2023-02-20")]),
@@ -32,24 +82,70 @@ ntl_df <- ntl_df %>%
          viirs_bm_gf_mean_14d_pc = (viirs_bm_gf_mean_14d - viirs_bm_gf_mean_jan)/viirs_bm_gf_mean_jan*100,
          viirs_bm_gf_mean_mar_pc = (viirs_bm_gf_mean_mar - viirs_bm_gf_mean_jan)/viirs_bm_gf_mean_jan*100)
 
-## Merge with GADM
-gadm_sp <- read_sf(file.path(unocha_dir, "RawData", "syr_admbnda_adm3_uncs_unocha_20201217.json")) %>%
+# Merge with polygons ----------------------------------------------------------
+
+#### ADM0 Polygons
+gadm_syr0_sf <- read_sf(file.path(unocha_dir, "RawData", "syr_admbnda_adm0_uncs_unocha_20201217.json")) 
+
+#### Polygons
+## Syr
+gadm_syr_sp <- read_sf(file.path(unocha_dir, "RawData", "syr_admbnda_adm3_uncs_unocha_20201217.json")) %>%
   as("Spatial")
-gadm_sp$date <- NULL
-gadm_sp <- merge(gadm_sp, ntl_df, by = "ADM3_PCODE")
+
+gadm_syr_sp@data <- gadm_syr_sp@data %>%
+  dplyr::mutate(adm = ADM3_PCODE) %>%
+  dplyr::select(adm) %>%
+  dplyr::mutate(country = "syr")
+
+## Tur
+gadm_tur_sp <- read_sf(dsn = file.path(data_tur_dir, 
+                                       "turkey_administrativelevels0_1_2", 
+                                       "tur_polbna_adm2.shp")) %>%
+  as("Spatial")
+
+gadm_tur_sp@data <- gadm_tur_sp@data %>%
+  dplyr::mutate(adm = pcode) %>%
+  dplyr::select(adm) %>%
+  dplyr::mutate(country = "tur")
+
+## Append
+dist <- st_distance(gadm_tur_sp %>% st_as_sf(), gadm_syr0_sf) %>% as.vector()
+gadm_tur_sp <- gadm_tur_sp[dist <= 40000,]
+
+gadm_sp <- rbind(gadm_tur_sp, gadm_syr_sp)
+
+gadm_sp <- merge(gadm_sp, ntl_df, by = "adm")
 
 gadm_sf <- gadm_sp %>% st_as_sf()
 
 # Map --------------------------------------------------------------------------
 gadm_sub_sf <- gadm_sf %>%
+  st_buffer(dist = 2000) %>%
   dplyr::filter(intensity %in% c("Strong", "Very Strong", "Moderate"))
 
 gadm_strong_sf <- gadm_sf %>%
   dplyr::filter(intensity %in% c("Very Strong", "Strong")) %>%
+  st_buffer(dist = 2000) %>%
   st_union()
 
 gadm_all_sf <- gadm_sf %>%
+  st_buffer(dist = 2000) %>%
   dplyr::filter(intensity %in% c("Very Strong", "Strong", "Moderate")) %>%
+  st_union()
+
+## Syr
+gadm_syr_sub_sf <- gadm_sf %>%
+  dplyr::filter(country == "syr",
+                intensity %in% c("Strong", "Very Strong", "Moderate"))
+
+gadm_syr_strong_sf <- gadm_sf %>%
+  dplyr::filter(country == "syr",
+                intensity %in% c("Very Strong", "Strong")) %>%
+  st_union()
+
+gadm_syr_all_sf <- gadm_sf %>%
+  dplyr::filter(country == "syr",
+                intensity %in% c("Very Strong", "Strong", "Moderate")) %>%
   st_union()
 
 # Just EQ Affected -------------------------------------------------------------
@@ -57,164 +153,198 @@ gadm_all_sf <- gadm_sf %>%
 #### 3 Days - focus
 mm <- gadm_sub_sf$viirs_bm_mean_3d_pc %>% abs() %>% max()
 
-ggplot() +
+p <- ggplot() +
   geom_sf(data = gadm_sub_sf,
           aes(fill = viirs_bm_mean_3d_pc),
           color = NA) +
-  # geom_sf(data = gadm_all_sf,
-  #         fill = NA,
-  #         color = "black") +
-  geom_sf(data = gadm_strong_sf,
+  geom_sf(data = gadm_syr_sub_sf %>% st_union(),
+          fill = NA,
+          aes(color = "Syria"),
+          size = 0.5) +
+  geom_sf(data = gadm_syr_strong_sf,
           fill = NA,
           color = "black") +
   theme_void() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
   labs(fill = "Percent\nChange\nNTL",
+       color = NULL,
        title = "Nighttime Lights Percent Change\nJan to 3 Days After Earthquake") +
   scale_fill_gradient2(low = "red",
                        mid = "white",
                        high = "forestgreen",
                        midpoint = 0,
-                       limits = c(-mm, mm)) 
+                       limits = c(-mm, mm)) +
+  scale_color_manual(values = c("dodgerblue3"))
 
-ggsave(filename = file.path(figures_dir, "map_ntl_eq_3d_strong.png"),
+ggsave(p, filename = file.path(figures_dir, "map_ntl_eq_3d_strong.png"),
        height = 4.5, 
        width = 4.5)
 
 #### 7 Days - focus
 mm <- gadm_sub_sf$viirs_bm_mean_14d_pc %>% abs() %>% max()
 
-ggplot() +
+p <- ggplot() +
   geom_sf(data = gadm_sub_sf,
           aes(fill = viirs_bm_mean_14d_pc),
           color = NA) +
-  # geom_sf(data = gadm_all_sf,
-  #         fill = NA,
-  #         color = "black") +
-  geom_sf(data = gadm_strong_sf,
+  geom_sf(data = gadm_syr_sub_sf %>% st_union(),
+          fill = NA,
+          aes(color = "Syria"),
+          size = 0.5) +
+  geom_sf(data = gadm_syr_strong_sf,
           fill = NA,
           color = "black") +
   theme_void() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
   labs(fill = "Percent\nChange\nNTL",
+       color = NULL,
        title = "Nighttime Lights Percent Change\nJan to 2 Weeks After Earthquake") +
   scale_fill_gradient2(low = "red",
                        mid = "white",
                        high = "forestgreen",
                        midpoint = 0,
-                       limits = c(-mm, mm))
+                       limits = c(-mm, mm)) +
+  scale_color_manual(values = c("dodgerblue3"))
 
-ggsave(filename = file.path(figures_dir, "map_ntl_eq_14d_strong.png"),
+ggsave(p,
+       filename = file.path(figures_dir, "map_ntl_eq_14d_strong.png"),
        height = 4.5, 
        width = 4.5)
 
 #### March - focus
 mm <- gadm_sub_sf$viirs_bm_mean_mar_pc %>% abs() %>% max()
 
-ggplot() +
+p <- ggplot() +
   geom_sf(data = gadm_sub_sf,
           aes(fill = viirs_bm_mean_mar_pc),
           color = NA) +
-  # geom_sf(data = gadm_all_sf,
-  #         fill = NA,
-  #         color = "black") +
-  geom_sf(data = gadm_strong_sf,
+  geom_sf(data = gadm_syr_sub_sf %>% st_union(),
+          fill = NA,
+          aes(color = "Syria"),
+          size = 0.5) +
+  geom_sf(data = gadm_syr_strong_sf,
           fill = NA,
           color = "black") +
   theme_void() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
   labs(fill = "Percent\nChange\nNTL",
+       color = NULL,
        title = "Nighttime Lights Percent Change\nJan to March 2023") +
   scale_fill_gradient2(low = "red",
                        mid = "white",
                        high = "forestgreen",
                        midpoint = 0,
-                       limits = c(-mm, mm))
+                       limits = c(-mm, mm)) +
+  scale_color_manual(values = c("dodgerblue3"))
 
-ggsave(filename = file.path(figures_dir, "map_ntl_eq_march_strong.png"),
+ggsave(p,
+       filename = file.path(figures_dir, "map_ntl_eq_march_strong.png"),
        height = 4.5, 
        width = 4.5)
 
 # All Syria --------------------------------------------------------------------
+# gadm_sf <- gadm_sf %>%
+#   dplyr::filter((country == "syr") |
+#                   (country == "tur") & (intensity %in% c("Strong",
+#                                                          "Very Strong",
+#                                                          "Moderate")))
 
 #### 3 Days - focus
 mm <- gadm_sf$viirs_bm_mean_3d_pc %>% abs() %>% max()
 
-ggplot() +
+p <- ggplot() +
   geom_sf(data = gadm_sf,
           aes(fill = viirs_bm_mean_3d_pc),
           color = NA) +
-  geom_sf(data = gadm_all_sf,
+  geom_sf(data = gadm_syr0_sf,
+          fill = NA,
+          aes(color = "Syria")) +
+  geom_sf(data = gadm_syr_all_sf,
           fill = NA,
           color = "black") +
-  geom_sf(data = gadm_strong_sf,
+  geom_sf(data = gadm_syr_strong_sf,
           fill = NA,
           color = "black") +
   theme_void() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
   labs(fill = "Percent\nChange\nNTL",
+       color = NULL,
        title = "Nighttime Lights Percent Change\nJan to 3 Days After Earthquake") +
   scale_fill_gradient2(low = "red",
                        mid = "white",
                        high = "forestgreen",
                        midpoint = 0,
-                       limits = c(-mm, mm))
+                       limits = c(-mm, mm)) +
+  scale_color_manual(values = c("dodgerblue3"))
 
-ggsave(filename = file.path(figures_dir, "map_ntl_eq_3d.png"),
+ggsave(p,
+       filename = file.path(figures_dir, "map_ntl_eq_3d.png"),
        height = 4.5, 
        width = 4.5)
 
 #### 14 Days - focus
 mm <- gadm_sf$viirs_bm_mean_14d_pc %>% abs() %>% max()
 
-ggplot() +
+p <- ggplot() +
   geom_sf(data = gadm_sf,
           aes(fill = viirs_bm_mean_14d_pc),
           color = NA) +
-  geom_sf(data = gadm_all_sf,
+  geom_sf(data = gadm_syr0_sf,
+          fill = NA,
+          aes(color = "Syria")) +
+  geom_sf(data = gadm_syr_all_sf,
           fill = NA,
           color = "black") +
-  geom_sf(data = gadm_strong_sf,
+  geom_sf(data = gadm_syr_strong_sf,
           fill = NA,
           color = "black") +
   theme_void() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
   labs(fill = "Percent\nChange\nNTL",
+       color = NULL,
        title = "Nighttime Lights Percent Change\nJan to 2 Weeks After Earthquake") +
   scale_fill_gradient2(low = "red",
                        mid = "white",
                        high = "forestgreen",
                        midpoint = 0,
-                       limits = c(-mm, mm))
+                       limits = c(-mm, mm)) +
+  scale_color_manual(values = c("dodgerblue3"))
 
-ggsave(filename = file.path(figures_dir, "map_ntl_eq_14d.png"),
+ggsave(p,
+       filename = file.path(figures_dir, "map_ntl_eq_14d.png"),
        height = 4.5, 
        width = 4.5)
 
 #### March - all
 mm <- gadm_sf$viirs_bm_mean_mar_pc %>% abs() %>% max()
 
-ggplot() +
+p <- ggplot() +
   geom_sf(data = gadm_sf,
           aes(fill = viirs_bm_mean_mar_pc),
           color = NA) +
-  geom_sf(data = gadm_all_sf,
+  geom_sf(data = gadm_syr0_sf,
+          fill = NA,
+          aes(color = "Syria")) +
+  geom_sf(data = gadm_syr_all_sf,
           fill = NA,
           color = "black") +
-  geom_sf(data = gadm_strong_sf,
+  geom_sf(data = gadm_syr_strong_sf,
           fill = NA,
           color = "black") +
   theme_void() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
   labs(fill = "Percent\nChange\nNTL",
+       color = NULL,
        title = "Nighttime Lights Percent Change\nJan to March 2023") +
   scale_fill_gradient2(low = "red",
                        mid = "white",
                        high = "forestgreen",
                        midpoint = 0,
-                       limits = c(-mm, mm))
+                       limits = c(-mm, mm)) +
+  scale_color_manual(values = c("dodgerblue3"))
 
-ggsave(filename = file.path(figures_dir, "map_ntl_eq_march.png"),
+ggsave(p,
+       filename = file.path(figures_dir, "map_ntl_eq_march.png"),
        height = 4.5, 
        width = 4.5)
 
@@ -223,80 +353,98 @@ ggsave(filename = file.path(figures_dir, "map_ntl_eq_march.png"),
 #### 3 Days - focus
 mm <- gadm_sf$viirs_bm_gf_mean_3d_pc %>% na.omit() %>% as.vector() %>% abs() %>% max()
 
-ggplot() +
+p <- ggplot() +
   geom_sf(data = gadm_sf,
           aes(fill = viirs_bm_gf_mean_3d_pc),
           color = NA) +
-  geom_sf(data = gadm_all_sf,
+  geom_sf(data = gadm_syr0_sf,
+          fill = NA,
+          aes(color = "Syria")) +
+  geom_sf(data = gadm_syr_all_sf,
           fill = NA,
           color = "black") +
-  geom_sf(data = gadm_strong_sf,
+  geom_sf(data = gadm_syr_strong_sf,
           fill = NA,
           color = "black") +
   theme_void() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
   labs(fill = "Percent\nChange\nNTL",
+       color = NULL,
        title = "Nighttime Lights Percent Change\nJan to 3 Days After Earthquake") +
   scale_fill_gradient2(low = "red",
                        mid = "white",
                        high = "forestgreen",
                        midpoint = 0,
-                       limits = c(-mm, mm))
+                       limits = c(-mm, mm)) +
+  scale_color_manual(values = c("dodgerblue3"))
 
-ggsave(filename = file.path(figures_dir, "map_ntl_gf_eq_3d.png"),
+ggsave(p,
+       filename = file.path(figures_dir, "map_ntl_gf_eq_3d.png"),
        height = 4.5, 
        width = 4.5)
 
 #### 14 Days - focus
 mm <- gadm_sf$viirs_bm_gf_mean_14d_pc %>% na.omit() %>% as.vector() %>% abs() %>% max()
 
-ggplot() +
+p <- ggplot() +
   geom_sf(data = gadm_sf,
           aes(fill = viirs_bm_gf_mean_14d_pc),
           color = NA) +
-  geom_sf(data = gadm_all_sf,
+  geom_sf(data = gadm_syr0_sf,
+          fill = NA,
+          aes(color = "Syria")) +
+  geom_sf(data = gadm_syr_all_sf,
           fill = NA,
           color = "black") +
-  geom_sf(data = gadm_strong_sf,
+  geom_sf(data = gadm_syr_strong_sf,
           fill = NA,
           color = "black") +
   theme_void() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
   labs(fill = "Percent\nChange\nNTL",
+       color = NULL,
        title = "Nighttime Lights Percent Change\nJan to 2 Weeks After Earthquake") +
   scale_fill_gradient2(low = "red",
                        mid = "white",
                        high = "forestgreen",
                        midpoint = 0,
-                       limits = c(-mm, mm))
+                       limits = c(-mm, mm)) +
+  scale_color_manual(values = c("dodgerblue3"))
 
-ggsave(filename = file.path(figures_dir, "map_ntl_gf_eq_14d.png"),
+ggsave(p,
+       filename = file.path(figures_dir, "map_ntl_gf_eq_14d.png"),
        height = 4.5, 
        width = 4.5)
 
 #### March - all
 mm <- gadm_sf$viirs_bm_gf_mean_mar_pc %>% na.omit() %>% as.vector() %>% abs() %>% max()
 
-ggplot() +
+p <- ggplot() +
   geom_sf(data = gadm_sf,
           aes(fill = viirs_bm_gf_mean_mar_pc),
           color = NA) +
-  geom_sf(data = gadm_all_sf,
+  geom_sf(data = gadm_syr0_sf,
+          fill = NA,
+          aes(color = "Syria")) +
+  geom_sf(data = gadm_syr_all_sf,
           fill = NA,
           color = "black") +
-  geom_sf(data = gadm_strong_sf,
+  geom_sf(data = gadm_syr_strong_sf,
           fill = NA,
           color = "black") +
   theme_void() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5)) +
   labs(fill = "Percent\nChange\nNTL",
+       color = NULL,
        title = "Nighttime Lights Percent Change\nJan to March 2023") +
   scale_fill_gradient2(low = "red",
                        mid = "white",
                        high = "forestgreen",
                        midpoint = 0,
-                       limits = c(-mm, mm))
+                       limits = c(-mm, mm)) +
+  scale_color_manual(values = c("dodgerblue3"))
 
-ggsave(filename = file.path(figures_dir, "map_ntl_gf_eq_march.png"),
+ggsave(p,
+       filename = file.path(figures_dir, "map_ntl_gf_eq_march.png"),
        height = 4.5, 
        width = 4.5)
